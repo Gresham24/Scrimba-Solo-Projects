259013023c83e950ab32f447dd9efe8a3e3f9ed1
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2022-02-24 11:12:11 +0200

    [Movie W.list] created watchlist.js

---
+++ Movie-watchlist/watchlist.js
@@ -0,0 +1,10 @@
+const searchBtn = document.getElementById('search-page-btn');
+const addMovieBtn = document.getElementById('add-movie-btn');
+
+searchBtn.addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+
+addMovieBtn.addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
