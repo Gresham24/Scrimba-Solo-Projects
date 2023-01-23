0ed432b64a432a858d1f0c0b248c0b09997bd0d6
Gresham Tembo <70582296+Gresham24@users.noreply.github.com>
2023-01-23 23:32:44 +0200

    [Movie W.L] test api call and eventListn.

---
+++ Movie-watchlist/index.js
@@ -0,0 +1,53 @@
+/*
+Features:
+- on index.html page, once movie is added to watchlist, change "+ watchlist" button to "added to watchlist"
+- 
+
+*/ 
+
+/* ============================
+            Variables
+=============================== */
+
+const searchQry = null;
+const poster = null;
+const title = null;
+const imdbRating = null;
+const runtime = null;
+const genre = null;
+const plot = null;
+
+
+
+/* ============================
+            Functions
+=============================== */
+
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=42f3cd82')
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data.Title)
+        // console.log(data.Poster)
+        // console.log(data.Runtime)
+        // console.log(data.imdbRating)
+        // console.log(data.Genre)
+        // console.log(data.Plot)
+    });
+
+
+
+
+
+/* ============================
+            EVENT LISTENERS
+=============================== */
+
+const searchInput = document.querySelector("input[type='search']");
+console.log(searchInput.value); 
+const searchValue = searchInput.value; 
+
+document.getElementById('search-btn').addEventListener('click', function() {
+    console.log('test print');
+    console.log(searchValue);
+})
+
